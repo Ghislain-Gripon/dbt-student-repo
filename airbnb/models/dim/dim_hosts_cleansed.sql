7ee8747988c231a53fdc6de39c62d77773e19ec0
Ghislain Gripon <ghislain.gripon@gmail.com>
2026-02-22 18:54:03 +0100

    additional sqlfmt config

--- airbnb/models/dim/dim_hosts_cleansed.sql
+++ airbnb/models/dim/dim_hosts_cleansed.sql
@@ -2,9 +2,7 @@ with src_hosts as (select * from {{ ref("src_hosts") }})
 
 select
     host_id,
-    coalesce(
-        case when host_name = '' then null else host_name end, 'Anonymous'
-    ) as host_name,
+    coalesce(case when host_name = '' then null else host_name end, 'Anonymous') as host_name,
     is_superhost,
     created_at,
     updated_at
--- airbnb/models/fct/fct_reviews.sql
+++ airbnb/models/fct/fct_reviews.sql
@@ -1,4 +1,9 @@
-{{ config(materialized="incremental", on_schema_change="fail") }}
+{{ 
+    config(
+        materialized="incremental", 
+        on_schema_change="fail"
+    ) 
+}}
 with src_reviews as (select * from {{ ref("src_reviews") }})
 select *
 from src_reviews
