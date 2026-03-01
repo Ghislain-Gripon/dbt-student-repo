4f39c403b580c6b43994968181bf77ffa5baaacf
Ghislain Gripon <ghislain.gripon@gmail.com>
2026-03-01 23:30:17 +0100

    dbtutils

--- airbnb/models/fct/fct_reviews.sql
+++ airbnb/models/fct/fct_reviews.sql
@@ -6,7 +6,7 @@
 }}
 with src_reviews as (select * from {{ ref("src_reviews") }})
 
-select s.*
+select {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} as review_id, s.* -- noqa
 from src_reviews as s
 where
     s.review_text is not null
